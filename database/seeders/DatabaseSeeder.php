1b6279a820159646cf3739479ea9106163f33c21
abdullohumar <iniautok@gmail.com>
2026-02-23 12:34:20 +0700

    feat(auth): setup database seeder for initial super admin and create auth controller API

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +18,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+    
+        Permission::firstOrCreate(['name' => 'manage_users']);
+        Permission::firstOrCreate(['name' => 'manage_products']);
+        Permission::firstOrCreate(['name' => 'manage_promos']);
+        Permission::firstOrCreate(['name' => 'view_all_transactions']);
+    
+        // Buat role
+        $superAdminRole = Role::firstOrCreate([
+            'name' => 'super_admin',
+            'is_system' => true,
         ]);
+
+        $karyawanRole = Role::firstOrCreate([
+            'name' => 'karyawan',
+            'is_system' => false
+        ]);
+
+        // Assign permission to super_admin
+        $superAdminRole->givePermissionTo(Permission::all());        
+    
+        // Akun super admin
+        $superAdmin = User::firstOrCreate(
+            ['email' => 'admin@laundry.com'],
+            [
+                'name' => 'Laundry Owner',
+                'password' => Hash::make('laundry_ganteng')
+            ]
+        );
+
+        $superAdmin->assignRole($superAdminRole);
     }
 }
