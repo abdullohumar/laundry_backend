2e4ed10e2a8e7eeb38f5a072d4443c910d83fc19
abdullohumar <iniautok@gmail.com>
2026-03-05 16:54:08 +0700

    chore(database): add dummy data for settings, products, and machines in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Machine;
+use App\Models\Product;
+use App\Models\Setting;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,12 +22,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
-    
+
         Permission::firstOrCreate(['name' => 'manage_users']);
         Permission::firstOrCreate(['name' => 'manage_products']);
         Permission::firstOrCreate(['name' => 'manage_promos']);
         Permission::firstOrCreate(['name' => 'view_all_transactions']);
-    
+
         // Buat role
         $superAdminRole = Role::firstOrCreate([
             'name' => 'super_admin',
@@ -37,8 +40,8 @@ class DatabaseSeeder extends Seeder
         ]);
 
         // Assign permission to super_admin
-        $superAdminRole->givePermissionTo(Permission::all());        
-    
+        $superAdminRole->givePermissionTo(Permission::all());
+
         // Akun super admin
         $superAdmin = User::firstOrCreate(
             ['email' => 'admin@laundry.com'],
@@ -49,5 +52,37 @@ class DatabaseSeeder extends Seeder
         );
 
         $superAdmin->assignRole($superAdminRole);
+
+        // 1. Buat Data Settings untuk Promo
+        Setting::firstOrCreate(['key' => 'promo_coin_target'], ['value' => '10']);
+        Setting::firstOrCreate(['key' => 'promo_reward_product_id'], ['value' => '2']); // ID 2 nanti adalah detergen
+
+        // 2. Buat Dummy Produk
+        Product::firstOrCreate(['id' => 1], [
+            'name' => 'Koin Laundry',
+            'type' => 'coin',
+            'price' => 10000,
+            'stock' => 9999,
+        ]);
+
+        Product::firstOrCreate(['id' => 2], [
+            'name' => 'Detergen Cair Saset',
+            'type' => 'addon',
+            'price' => 2000,
+            'stock' => 100,
+        ]);
+
+        // 3. Buat Dummy Mesin
+        Machine::firstOrCreate(['id' => 1], [
+            'code' => 'W1', // Washer 1
+            'type' => 'washer',
+            'status' => 'idle',
+        ]);
+
+        Machine::firstOrCreate(['id' => 2], [
+            'code' => 'D1', // Dryer 1
+            'type' => 'dryer',
+            'status' => 'idle',
+        ]);
     }
 }
